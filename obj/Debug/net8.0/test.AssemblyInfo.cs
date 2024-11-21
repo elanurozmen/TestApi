7ff873a94c290574bb35a89cba4624e412f05cfb
Elanur  Özmen <200201110@kocaeli.edu.tr>
2024-11-21 14:58:53 +0300

    t

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7e9347068a4bec9aba39bf22385c452b38d460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6c52308a0cb1069940c761c1823057c684f662")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
